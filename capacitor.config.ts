3cc752b0761260059e5ecb94e63f4ac1a4af75d7
ebgarcia <ebgarcia@sudamericano.edu.ec>
2025-05-10 18:35:09 -0500

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Plugins',
-  webDir: 'www'
+  appId: 'com.miapp.capacitor',
+  appName: 'Mi App Capacitor',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      spinnerColor: "#999999",
+    },
+  },
+  android: {
+    allowMixedContent: true
+  }
 };
 
 export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Network } from '@capacitor/network';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+   styleUrls: ['home.page.scss'],
+  imports: [IonicModule, CommonModule],
 })
+
 export class HomePage {
-  constructor() {}
+  location: any = null;
+  imageUrl: string | undefined;
+  networkStatus: any;
+
+  async getLocation() {
+    try {
+      const pos = await Geolocation.getCurrentPosition();
+      this.location = pos.coords;
+    } catch (err) {
+      console.error('Error al obtener ubicación', err);
+    }
+  }
+
+  async takePicture() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera
+      });
+      this.imageUrl = image.webPath;
+    } catch (err) {
+      console.error('Error al tomar la foto', err);
+    }
+  }
+
+  async checkNetwork() {
+    try {
+      const status = await Network.getStatus();
+      this.networkStatus = status;
+      console.log('Estado de red:', status);
+    } catch (err) {
+      console.error('Error al obtener estado de red', err);
+    }
+  }
 }
